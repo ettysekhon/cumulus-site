dfe53e95214d34bd83347f5bdc7790fe4e5ef3f1
Etty Sekhon <etty.sekhon@gmail.com>
2025-04-13 13:11:31 +0100

    Fix ensure robots.txt is available

--- config/settings.py
+++ config/settings.py
@@ -146,3 +146,5 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [BASE_DIR / "core" / "static"]
